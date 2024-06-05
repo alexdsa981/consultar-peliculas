e6af600b2fde125487986ec94155270193a7b1e6
Alex Saldaña <alexdsa981@gmail.com>
2024-06-05 17:07:07 -0500

    agrega dependencia jackson databind, agrega paquete service con clase ConsumoAPI

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,23 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ScreenmatchApplication.class, args);
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoAPI consumoApi = new ConsumoAPI();
+		String json = consumoApi.obtenerDatos("https://www.omdbapi.com/?apikey=7880a47c&t=breaking+bad");
+		System.out.println(json);
+	}
 }
